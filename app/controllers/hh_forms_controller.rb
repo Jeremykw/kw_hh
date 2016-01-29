a01b4f3b61efe69a2582c09c1d8fd5b77f42eb18
Jeremy Bissonnette <jeremy@kwmassage.com>
2016-01-29 14:57:55 -0500

    working with routing

--- app/controllers/hh_forms_controller.rb
+++ app/controllers/hh_forms_controller.rb
@@ -1,8 +1,24 @@
-class HhFormsController  < ApplicationController	
+class HhFormsController < ApplicationController
+	skip_before_action :verify_authenticity_token
 	def new
+		@hhform = HhForm.new
 	end
 
 	def create
+		if HhForm.create(hhform_params).invalid?
+			redirect_to hh_form_path(hhform_params)
+		else
+			redirect_to conformaton_path
+		end
 	end
 
+	###
+	# Strong Parameters
+	###
+
+	def hhform_params
+		if params[:hhform]
+			params.require(:hhform).permit!
+		end
+	end
 end
