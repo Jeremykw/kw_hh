ad789898c8fecf99767a8d26eaa5f8d00d735452
Jeremy Bissonnette <jeremy@kwmassage.com>
2016-02-03 05:17:30 -0500

    show view hh_form_fields

--- app/helpers/hh_forms_helper.rb
+++ app/helpers/hh_forms_helper.rb
@@ -2,6 +2,38 @@ module HhFormsHelper
 	###
 	# => Filters out feilds that should not be displayed in show
 	###
+	
+	def hh_form_fields(hhform, action)
+		if action == "personal_info"
+			personal_info(hhform)
+		end
+	end
+
+	def personal_info(hhform)
+		form_section = []
+		preg = hhform[:pregnant]
+		hhform.attributes.each do |field, data|
+			if field_has_data(field, data, preg) && title_should_be_shown(field, preg)
+				form_section << [field, data]
+			end
+			break if field == "date_of_birth"
+		end
+		form_section
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	def field_has_data(title, field, pregnant)
 		if field_data_is_true(field) && title_should_be_shown(title, pregnant)
 			
