88d11d81379cd88d11ad23e67127773c35a49c40
jeremykw <jeremy@kwmassage.com>
2016-02-27 18:10:03 -0500

    adding button

--- app/assets/javascripts/hh_form.js
+++ app/assets/javascripts/hh_form.js
@@ -6,29 +6,53 @@ function init(){
 	};
 };
 
-function hhForm(){
-	var state = getState(state);
+function hhForm(currentState, action){
+	var state = getState(currentState);
 	visabilityFilter(state);
-
-	function getState(state){
-		if(typeof state === 'undefined'){
-			var state = {
-				page: 0,	
-				form: []
-			};
-			return state;
+	// debugger
+	function getState(currentState){
+		var newState = new Object
+		if(typeof currentState === 'undefined'){
+				newState.page = 0;
+				newState.formInfo =  [];
+				addButton(newState);
+			}else{
+				newState.page = currentState.page + 1;
+				newState.formInfo = [];
+				addButton(newState);
 		};
+		return newState;
+
 	};
 
-	function visabilityFilter(state){
-		if(state.page === 0){
+	function visabilityFilter(currentState){
+		if(currentState.page === 0){
 			document.getElementById("complaints").remove();
 			document.getElementById("checkboxes").remove();
 			document.getElementById("concent").remove();
-			document.getElementById("other").remove();
 			document.getElementById("pains").remove();
+			document.getElementById("other").remove();
 
+		}else if(currentState.page === 1){
+			document.getElementById("contact").remove();
+			document.getElementById("checkboxes").remove();
+			document.getElementById("concent").remove();
+			document.getElementById("pains").remove();
+			document.getElementById("other").remove();
 		};
+
+	};
+
+	function addButton(state){
+		
+	    var element = document.createElement("button");
+	    element.type = "button";
+	    element.class = "btn btn-danger";
+	    element.value = "Save and Contunue";
+	    element.name = "button";
+	    element.onclick = "hhForm(state, 1)";
+	    debugger
+	    document.getElementById("contact").appendChild(element);
 
 	};
 };
