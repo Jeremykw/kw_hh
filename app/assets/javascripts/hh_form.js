b2c7a9cd3116faced76f7b2b08e3523f4f922a34
jeremykw <jeremy@kwmassage.com>
2016-02-29 21:32:18 -0500

    auto loading functions

--- app/assets/javascripts/hh_form.js
+++ app/assets/javascripts/hh_form.js
@@ -1,98 +1,119 @@
-window.onload = initHhForm;
-function initHhForm(){
-	var isNewHhForm = document.getElementById("hh_form");
-	if (isNewHhForm) {
-		var fullForm = hhForm.getFullForm();
-		var state = hhForm.initState();
-		var action = 'init';
-		hhForm.dispatch(state, action, fullForm);
+window.onload = (function(){
+	
+	var HhForm = {
 
-	};
+		init: function(){
+			this.fullForm = this.getFullForm;
+			this.state = {
+				page: 0,
+				formDate: []
+			};
+			this.nextAction = 'init';
+			this.visabilityFilter(this.state, this.fullForm)
+		},
 
-};
+		visabilityFilter: function(currentState, fullForm){
+			if(currentState.page === 0){
+				
+				document.getElementById("complaints").remove();
+				document.getElementById("checkboxes").remove();
+				document.getElementById("concent").remove();
+				document.getElementById("pains").remove();
+				document.getElementById("other").remove();
+				this.addButton(currentState, 'next', fullForm);
 
-var hhForm = {
-	dispatch: function(state, action, fullForm){
-		var newState;
-		if (action !== 'init'){
-			newState = this.getState(state, action)
-		}else{
-			newState = state
-		}
-		return this.visabilityFilter(newState, fullForm)
-		// switch(state){
-		// 	case 'init':
-		// 		visabilityFilter.init()
-		// 		break
-		// 	case 'forware':
-		// 		//
-		// 		break
-		// 	case 'back':
-		// 		//
-		// 		break
-		// 	default:
-	},
+			}else if(currentState.page === 1){
+				document.getElementById("contact").remove();
+				document.getElementById("complaints").add();
+			};
+
+		},
+
+		getFullForm: {
+				concent: document.getElementById("concent"),
+				complaints: document.getElementById("complaints"),
+				checkboxes: document.getElementById("checkboxes"),
+				pains: 		document.getElementById("pains"),
+				other: 		document.getElementById("other"),
+				concent: 	document.getElementById("concent")
+		},
 
 
-	getState: function(currentState, action){
-		var newState = new Object;
-		if(currentState.page === []){
-				// removed init state
-			}else{
-				newState.page = currentState.page + 1;
-				newState.formData = [];
-		};
-		return newState;
+		addButton: function(state, action, fullForm){
+			var button = document.getElementById("put_contact_button_here")
+		    var element = document.createElement("input");
+		    element.type = "button";
+		    element.setAttribute( 'value', 'Save and Contunue');
+		    element.setAttribute('class', "btn btn-danger topmargin-sm");
+		    element.setAttribute('onclick', '(this.state, action, this.fullForm)');
+		    // debugger
 
-	},
+		    button.appendChild(element);
 
-	visabilityFilter: function(currentState, fullForm){
-		if(currentState.page === 0){
-			document.getElementById("complaints").remove();
-			document.getElementById("checkboxes").remove();
-			document.getElementById("concent").remove();
-			document.getElementById("pains").remove();
-			document.getElementById("other").remove();
-			this.addButton('next');
+		},
 
-		}else if(currentState.page === 1){
-			document.getElementById("contact").remove();
-			document.getElementById("complaints").add();
-		};
 
-	},
+	}
+	HhForm.init();
+});
 
-	addButton: function(state, action, fullForm){
-		var button = document.getElementById("put_contact_button_here")
-	    var element = document.createElement("input");
-	    element.type = "button";
-	    element.setAttribute( 'value', 'Save and Contunue');
-	    element.setAttribute('class', "btn btn-danger topmargin-sm");
-	    element.setAttribute('onclick', 'hhForm.dispatch(state, action, fullForm)');
-	    // debugger
 
-	    button.appendChild(element);
+// function initHhForm(){
+// 	var isNewHhForm = document.getElementById("hh_form");
+// 	if (isNewHhForm) {
+// 		var fullForm = getFullForm();
+// 		var state = initState();
+// 		var action = 'init';
+// 		dispatch(state, action, fullForm);
 
-	},
+// 	};
 
-	getFullForm: function(){
-		var initFullForm = {
-			concent: document.getElementById("concent"),
-			complaints: document.getElementById("complaints"),
-			checkboxes: document.getElementById("checkboxes"),
-			pains: 		document.getElementById("pains"),
-			other: 		document.getElementById("other"),
-			concent: 	document.getElementById("concent")
-		};
-		return initFullForm;
-	},
 
-	initState: function(){
-		var initialState = {
-			page: 0,
-			formData: []
-		};
-		return initialState;
-	},
 
-};
+
+
+// 		function dispatch (state, action, fullForm){
+// 			var newState;
+// 			if (action === 'init'){
+// 				newState = state
+// 			}else{
+// 				newState = getState(state, action)
+// 			}
+// 			return visabilityFilter(newState, fullForm)
+// 			// switch(state){
+// 			// 	case 'init':
+// 			// 		visabilityFilter.init()
+// 			// 		break
+// 			// 	case 'forware':
+// 			// 		//
+// 			// 		break
+// 			// 	case 'back':
+// 			// 		//
+// 			// 		break
+// 			// 	default:
+// 		};
+
+
+// 		function getState(currentState, action){
+// 			var newState = new Object;
+// 			if(currentState.page === []){
+// 					// removed init state
+// 				}else{
+// 					newState.page = currentState.page + 1;
+// 					newState.formData = [];
+// 			};
+// 			return newState;
+
+// 		};
+
+
+
+// 		function initState(){
+// 			var initialState = {
+// 				page: 0,
+// 				formData: []
+// 			};
+// 			return initialState;
+// 		};
+
+// };
