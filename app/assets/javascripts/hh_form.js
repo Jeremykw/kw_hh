8ca414de33bcbbaac73273020ab3bcb7247a9e05
Jeremy Bissonnette <jeremy@localhost.localdomain>
2016-03-01 20:15:24 -0500

    objects

--- app/assets/javascripts/hh_form.js
+++ app/assets/javascripts/hh_form.js
@@ -2,49 +2,46 @@ window.onload = init;
 function init(){
 	var isNewHhForm = document.getElementById("hh_form");
 	if (isNewHhForm) {
-		this.fullForm = getFullForm();
-		this.formState = hhForm.init();
-		this.formAction = 'init';
-		hhDispatch(this.formState, this.formAction);
+		var HHFORM = new hhForm();
+		dispatch(HHFORM, "init");
 
 	};
 
 };
 
-function hhDispatch(formState, formAction){
-	var newState;
-	if (formAction === 'init'){
-		newState = formState;
-	
+
+
+function dispatch(form, action){
+	var newForm;
+	if (action === 'init'){
+		newForm = form;
 	}else{
-
-		newState = hhForm.getState(formState, formAction);
-	debugger
-
+		newForm = new hhForm(form.getState(action));
+		debugger
 		// this.state = this.newState;
 	}
-	
-	return hhForm.visabilityFilter(newState, fullForm);
-		
-
+	return newForm.visabilityFilter();
 };
 
+function hhForm(newState) {
+	this.state = {
+		page: 0,
+		formDate: [],
+		fullForm: function(){
+			var initFullForm = {
+				concent: document.getElementById("concent"),
+				complaints: document.getElementById("complaints"),
+				checkboxes: document.getElementById("checkboxes"),
+				pains: 		document.getElementById("pains"),
+				other: 		document.getElementById("other"),
+				concent: 	document.getElementById("concent")
+			};
+			return initFullForm;
+		}
+	};
 
 
-
-var hhForm = {
-
-	init: function(){
-		var initialState = {
-			page: 0,
-			formData: []
-		};
-		return initialState;
-
-	},
-
-	getState: function(formState, formAction){
-
+	this.getState = function(action){
 		if( formAction === 'next' ){
 			formState = {
 				page: formState.page + 1,
@@ -55,23 +52,20 @@ var hhForm = {
 			formState = {
 				page: formState.page - 1,
 				formData: []
-
 			}
-			
 		};
-
 		return formState;
+	};
 
-	},
 
-	visabilityFilter: function(formState, fullForm){
-		if(formState.page === 0){
+	this.visabilityFilter = function(){
+		if(this.state.page === 0){
 			document.getElementById("complaints").remove();
 			document.getElementById("checkboxes").remove();
 			document.getElementById("concent").remove();
 			document.getElementById("pains").remove();
 			document.getElementById("other").remove();
-			this.addNextButton(formState, fullForm);
+			addNextButton();
 
 		}else if(formState.page === 1){
 			document.getElementById("contact").remove();
@@ -82,43 +76,33 @@ var hhForm = {
 			this.addBackButton(formState, fullForm);
 		};
 
-	},
+	};
 
-	addNextButton: function(formState, fullForm){
+	addNextButton = function(){
 
 		var button = document.getElementById("put_contact_button_here")
 	    var element = document.createElement("input");
 	    element.type = "button";
 	    element.setAttribute( 'value', 'Save and Contunue');
 	    element.setAttribute('class', "btn btn-danger topmargin-sm rightmargin-sm");
-	    element.setAttribute('onclick', 'hhDispatch(formState, "next")');
+	    element.setAttribute('onclick', 'dispatch(this.state, "next")');
 	    
 	    button.appendChild(element);
-	},
+	};
 
-	addBackButton: function(formState, fullForm){
+	this.addBackButton = function(formState, fullForm){
 
 		var button = document.getElementById("put_contact_button_here")
 	    var element = document.createElement("input");
 	    element.type = "button";
 	    element.setAttribute( 'value', 'Back');
 	    element.setAttribute('class', "btn btn-danger topmargin-sm rightmargin-sm");
-	    element.setAttribute('onclick', 'hhDispatch(formState, "Back")');
+	    element.setAttribute('onclick', 'dispatch(this, "Back")');
 	    
 	    button.appendChild(element);
-	},
-
-};
-
-
-function getFullForm() {
-	var f = {
-	concent: document.getElementById("concent"),
-	complaints: document.getElementById("complaints"),
-	checkboxes: document.getElementById("checkboxes"),
-	pains: 		document.getElementById("pains"),
-	other: 		document.getElementById("other"),
-	concent: 	document.getElementById("concent")
 	};
-	return f;	
+
+
 };
+
+
